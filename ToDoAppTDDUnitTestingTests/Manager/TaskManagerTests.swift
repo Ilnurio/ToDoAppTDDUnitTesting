cf270ad6d33bbe75d4dff46c67ed9244bb0bb737
Ilnur Fattakhov <nurich1995@mail.ru>
2023-10-31 20:08:43 +0300

    testWhenTaskManagerRecreatedSavedShouldBeEqual()

--- ToDoAppTDDUnitTestingTests/Manager/TaskManagerTests.swift
+++ ToDoAppTDDUnitTestingTests/Manager/TaskManagerTests.swift
@@ -18,6 +18,7 @@ final class TaskManagerTests: XCTestCase {
     }
 
     override func tearDownWithError() throws {
+        sut.removeAll()
         super.tearDown()
         sut = nil
     }
@@ -96,5 +97,24 @@ final class TaskManagerTests: XCTestCase {
         sut.add(task: Task(title: "Foo"))
         
         XCTAssertTrue(sut.tasksCount == 1)
+    }
+    
+    func testWhenTaskManagerRecreatedSavedShouldBeEqual() {
+        var taskManager: TaskManager! = TaskManager()
+        let task = Task(title: "Foo")
+        let task1 = Task(title: "Bar")
+        
+        taskManager.add(task: task)
+        taskManager.add(task: task1)
+        
+        NotificationCenter.default.post(name: UIApplication.willResignActiveNotification, object: nil)
+        
+        taskManager = nil
+        
+        taskManager = TaskManager()
+        
+        XCTAssertEqual(taskManager.tasksCount, 2)
+        XCTAssertEqual(taskManager.task(at: 0), task)
+        XCTAssertEqual(taskManager.task(at: 1), task1)
     }
 }
